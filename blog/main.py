75042c271d4d58984c3862de196c193cce0d6d82
Arun Misra <arunmisra2273@gmail.com>
2021-06-13 10:14:09 +0530

    Store data in DB

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.param_functions import Body
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')
-def create(blog: schemas.Blog):
-    return blog
+def create(blog: schemas.Blog, db: Session=Depends(get_db)):
+    newBlog = models.Blog(title=blog.title, body=blog.body)
+    db.add(newBlog)
+    db.commit()
+    db.refresh(newBlog)
+    return newBlog
